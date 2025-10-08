6357a7be504d52da31e761932146edee9d283fd0
Dipanjan441 <dipanjantripathi441@gmail.com>
2025-10-08 09:54:42 +0530

    UI changes

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { MessageState, Sender } from './types';
 import { getFormattedMessageData, handleResponse } from './utils';
 import ReactMarkdown from "react-markdown";
@@ -12,6 +12,13 @@ const page = () => {
     const [messages, setMessages] = useState<MessageState[]>([]);
     const [input, setInput] = useState("");
     const [loading, setLoading] = useState(false);
+
+    //for scroll trigger
+    const scrollTriggerRef = useRef<HTMLDivElement | null>(null)
+
+    useEffect(()=>{
+        scrollTriggerRef.current?.scrollIntoView({behavior: "smooth"});
+    },[messages]);
 
     const handleSend = async (data: string) => {
         if (!data.trim()) return;
@@ -26,7 +33,6 @@ const page = () => {
 
         // Stream response
         await handleResponse(updatedMsg, (chunk) => {
-            console.log(chunk)
             setMessages((prev) =>
                 prev.map((msg) =>
                     msg.id === botMessage.id
@@ -76,45 +82,47 @@ const page = () => {
                                     </div>
                                 )}
                             </div>
-                            </div>
-                            )
+                        </div>
+                    )
                 }) : null}
-                        </div>
+                {/* empty div for scroll trigger  */}
+                <div ref={scrollTriggerRef} />
+            </div>
 
-            {/* Input Bar */ }
-                    <div className="border-t p-3 bg-white shrink-0 sticky bottom-0">
-                        <div className="flex items-center gap-2">
-                            <textarea
-                                // type="text"
-                                placeholder="Type a message..."
-                                value={input}
-                                onChange={(e) => {
-                                    setInput(e.target.value);
-                                    // auto-resize
-                                    e.target.style.height = "auto"; // reset height
-                                    e.target.style.height = e.target.scrollHeight + "px"; // grow with content
-                                }
-                                }
-                                rows={1}
-                                onKeyDown={(e) => {
-                                    if (e.key === "Enter" && !e.shiftKey) {
-                                        e.preventDefault();
-                                        handleSend(input);
-                                    }
-                                }}
-                                className="flex-1 border rounded-lg px-3 py-2 focus:outline-none overflow-hidden"
-                            />
-                            <button
-                                onClick={() => handleSend(input)}
-                                className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
-                                disabled={loading}
-                            >
-                                Send
-                            </button>
-                        </div>
-                    </div>
+            {/* Input Bar */}
+            <div className="border-t p-3 bg-white shrink-0 sticky bottom-0">
+                <div className="flex items-center gap-2">
+                    <textarea
+                        // type="text"
+                        placeholder="Type a message..."
+                        value={input}
+                        onChange={(e) => {
+                            setInput(e.target.value);
+                            // auto-resize
+                            e.target.style.height = "auto"; // reset height
+                            e.target.style.height = e.target.scrollHeight + "px"; // grow with content
+                        }
+                        }
+                        rows={1}
+                        onKeyDown={(e) => {
+                            if (e.key === "Enter" && !e.shiftKey) {
+                                e.preventDefault();
+                                handleSend(input);
+                            }
+                        }}
+                        className="flex-1 border rounded-lg px-3 py-2 focus:outline-none overflow-hidden"
+                    />
+                    <button
+                        onClick={() => handleSend(input)}
+                        className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+                        disabled={loading}
+                    >
+                        Send
+                    </button>
+                </div>
+            </div>
         </div>
-            );
+    );
 }
 
-            export default page
+export default page
